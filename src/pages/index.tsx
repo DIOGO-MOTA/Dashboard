9d6824ff2a8a351b6fea7b46939924351740737d
DIOGO-MOTA <diogomota21@gmail.com>
2021-08-02 21:50:32 -0300

    ReacForm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import { Flex,  Button, Stack } from "@chakra-ui/react";
-import { Input } from "../components/Form/Input"
+import { Input } from "../components/Form/Input";
+import { SubmitHandler, useForm} from 'react-hook-form';
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+
+
+ // const { errors } = formState
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (value) =>{
+    await new  Promise(resolve => setTimeout(resolve, 2000));
+    console.log(value)
+  }
+
   return (
     <Flex
       w="100vm"
@@ -17,13 +34,31 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input 
+           name="email" 
+           type="email"
+           label="E-mail"
+           {...register('email')}
+            />
+          <Input 
+           name="password" 
+           type="password" 
+           label="Senha"    
+           ref={register} 
+           {...register('password')}
+           />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="pink" 
+          size="lg"
+          isLoading={formState.isSubmitting}
+          >
           Entrar
        </Button>
 
