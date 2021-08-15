71efe8feecc6f014aa2dde6d05979dfed6217072
DIOGO-MOTA <diogomota21@gmail.com>
2021-08-15 11:45:55 -0300

    logicaPaginacao

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,17 +1,23 @@
-import { Box, Button, Flex, Heading, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine } from "react-icons/ri";
+
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 
 export default function UserList() {
+  const { data, isLoading, isFetching, error } = useUsers();
+
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
+
   return (
     <Box>
       <Header />
@@ -21,65 +27,74 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center" >
-            <Heading size="lg" fontWeight="normal">Usuàrios</Heading>
-           
-           <Link href="users/create" passHref>
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
 
-            >
-              Criar novo
-           </Button>
-           </Link>
+            <Heading size="lg" fontWeight="normal">
+              Usuàrios
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+            </Heading>
+
+            <Link href="users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
-          <Table colorSheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="green.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuàrio</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Diogo mota</Text>
-                    <Text fontSize="sm" color="gray.300">diogomota1@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>02 de Abril, 2021</Td>}
-              
-              </Tr>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Fala ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorSheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="green.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuàrio</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Kamila mota</Text>
-                    <Text fontSize="sm" color="gray.300">diogomota2221@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>02 de Abril, 2021</Td>}
-                
-              </Tr>
-            </Tbody>
-          </Table>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name} </Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
 
-          <Pagination />
+                      </Tr>
+                    )
+                  })}
+
+                </Tbody>
+              </Table>
+
+              <Pagination />
+            </>
+
+          )}
+
         </Box>
       </Flex>
 
